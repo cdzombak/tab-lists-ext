689583916fa28768a27824301a1b9dffc9d85362
Chris Dzombak <chris@dzombak.com>
2023-04-26 11:21:56 -0400

    upscale icon

--- util/convert-images.sh
+++ util/convert-images.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
 convert ./artworks/icon.png -density 144 -units PixelsPerInch -geometry 128x128 ./src/images/icon-128.png
 convert ./artworks/icon.png -density 144 -units PixelsPerInch -geometry 48x48 ./src/images/icon-48.png
+convert ./artworks/icon.png -density 144 -units PixelsPerInch -geometry 32x32 ./src/images/icon-32.png
 convert ./artworks/icon.png -density 144 -units PixelsPerInch -geometry 16x16 ./src/images/icon-16.png
